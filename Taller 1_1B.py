c7ebe9ae90e4a2039177275618ea5fb2a4bae1c6
lauravestupinanv-png <laurav.estupinanv@ecci.edu.co>
2026-08-18 18:55:35 -0500

    Create Taller 1_1B.py

---
+++ Taller 1_1B.py
@@ -0,0 +1,11 @@
+#1B. Potencia eléctrica
+
+# Entrada de datos
+voltaje = float(input("Ingrese el voltaje en voltios (V): "))
+corriente = float(input("Ingrese la corriente en amperios (A): "))
+
+# Cálculo de potencia
+potencia = voltaje * corriente
+
+# Resultado
+print("\nLa potencia consumida es:", potencia, "W")
