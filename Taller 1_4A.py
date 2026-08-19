d9029e342c8326dd7ec9603098953165698e1c17
lauravestupinanv-png <laurav.estupinanv@ecci.edu.co>
2026-08-18 19:26:59 -0500

    Update Taller 1_4A.py

--- Taller 1_4A.py
+++ Taller 1_4A.py
@@ -1,15 +1,35 @@
 #4A. PT100
 
-# Datos de la PT100
-R0 = 100
-A = 3.9083e-3
-B = -5.775e-7
+R0 = 100          # Resistencia a 0 °C (100 ohmios)
+A = 3.9083e-3     # Coeficiente A
+B = -5.775e-7     # Coeficiente B
+C = -4.183e-12    # Coeficiente C
 
-# Temperatura
-T = 100
+# Temperatura predeterminada
+T = 25
 
-# Cálculo de resistencia
-R = R0 * (1 + A*T + B*T**2)
+# Cálculo de la resistencia usando
+# la ecuación de Callendar-Van Dusen
 
-print("Temperatura:", T, "°C")
-print("Resistencia de la PT100:", R, "ohm")
+if T >= 0:
+    # Para temperaturas iguales o mayores a 0 °C
+    R = R0 * (
+        1
+        + A * T
+        + B * T**2
+    )
+
+else:
+    # Para temperaturas menores a 0 °C
+    R = R0 * (
+        1
+        + A * T
+        + B * T**2
+        + C * (T - 100) * T**3
+    )
+
+# Mostrar el resultado
+print(
+    f"La resistencia de la RTD PT100 a {T} °C es: "
+    f"{R:.4f} ohmios"
+)
