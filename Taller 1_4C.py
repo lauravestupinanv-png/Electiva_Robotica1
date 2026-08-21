2dbd78734af4870a563d2fdbcc8ef3f25e299bc6
lauravestupinanv-png <laurav.estupinanv@ecci.edu.co>
2026-08-20 19:16:33 -0500

    Update Taller 1_4C.py

--- Taller 1_4C.py
+++ Taller 1_4C.py
@@ -1,42 +1,117 @@
-#4C. Sistema coordenado X, Y, Z
+# C4. SISTEMA DE COORDENADAS X, Y, Z
 
-import numpy as np
 import matplotlib.pyplot as plt
 
+print(" SISTEMA DE COORDENADAS 3D")
+
+
 # Ingresar coordenadas del vector
-x = float(input("Ingrese la coordenada X: "))
-y = float(input("Ingrese la coordenada Y: "))
-z = float(input("Ingrese la coordenada Z: "))
+x = float(input("Ingrese la coordenada X del vector: "))
+y = float(input("Ingrese la coordenada Y del vector: "))
+z = float(input("Ingrese la coordenada Z del vector: "))
+
 
 # Crear figura 3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+fig = plt.figure(figsize=(8, 8))
+
+ax = fig.add_subplot(
+111,
+projection="3d"
+)
+
 
 # Tamaño de los ejes
-limite = max(abs(x), abs(y), abs(z)) + 2
+limite = max(
+abs(x),
+abs(y),
+abs(z),
+1
+) + 2
 
-# Ejes X, Y y Z
-ax.plot([0, limite], [0, 0], [0, 0])
-ax.plot([0, 0], [0, limite], [0, 0])
-ax.plot([0, 0], [0, 0], [0, limite])
 
-# Vector desde el origen hasta (x,y,z)
-ax.quiver(0, 0, 0, x, y, z)
+# EJES X, Y Y Z
+
+
+# Eje X
+ax.plot(
+[-limite, limite],
+[0, 0],
+[0, 0]
+)
+
+# Eje Y
+ax.plot(
+[0, 0],
+[-limite, limite],
+[0, 0]
+)
+
+# Eje Z
+ax.plot(
+[0, 0],
+[0, 0],
+[-limite, limite]
+)
+
+# VECTOR
+
+
+ax.quiver(
+0,
+0,
+0,
+x,
+y,
+z,
+arrow_length_ratio=0.1
+)
+
 
 # Punto final del vector
-ax.scatter(x, y, z)
+ax.scatter(
+x,
+y,
+z
+)
 
-# Etiquetas de los ejes
-ax.set_xlabel("X")S
+
+# Mostrar coordenadas del punto final
+ax.text(
+x,
+y,
+z,
+f"({x}, {y}, {z})"
+)
+
+
+# CONFIGURACIÓN
+
+
+ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
 
-# Límites
-ax.set_xlim(0, limite)
-ax.set_ylim(0, limite)
-ax.set_zlim(0, limite)
+# Valores positivos y negativos
+ax.set_xlim(
+-limite,
+limite
+)
 
-# Título
-ax.set_title("Sistema de coordenadas 3D y vector")
+ax.set_ylim(
+-limite,
+limite
+)
+
+ax.set_zlim(
+-limite,
+limite
+)
+
+# Quitar cuadrícula
+ax.grid(False)
+
+ax.set_title(
+"Sistema de coordenadas X, Y, Z y vector"
+)
 
 plt.show()
